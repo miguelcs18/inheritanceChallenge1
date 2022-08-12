5e26a8ac1dc5503983f527e17c349971d8036b51
miguelcs18 <miguelcs18@gmail.com>
2022-08-12 09:54:12 -0500

    Constructor and getters and setters for class Vehicle

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -3,4 +3,14 @@ public class Vehicle {
     int numberOfDoors;
     int numberOfGears;
     String Color;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(int numberOfWheels, int numberOfDoors, int numberOfGears, String color) {
+        this.numberOfWheels = numberOfWheels;
+        this.numberOfDoors = numberOfDoors;
+        this.numberOfGears = numberOfGears;
+        Color = color;
+    }
 }
