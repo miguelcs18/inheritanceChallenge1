9b1dfa9593802fba4110ce0fa652a2269529e512
miguelcs18 <miguelcs18@gmail.com>
2022-08-12 09:54:56 -0500

    Getters and setters for class Vehicle

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -13,4 +13,36 @@ public class Vehicle {
         this.numberOfGears = numberOfGears;
         Color = color;
     }
+
+    public int getNumberOfWheels() {
+        return numberOfWheels;
+    }
+
+    public void setNumberOfWheels(int numberOfWheels) {
+        this.numberOfWheels = numberOfWheels;
+    }
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public void setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public int getNumberOfGears() {
+        return numberOfGears;
+    }
+
+    public void setNumberOfGears(int numberOfGears) {
+        this.numberOfGears = numberOfGears;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
 }
